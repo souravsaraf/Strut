e3a8e84bb5c41224f5edfb4de614cdb9a7414819
souravsaraf <souravsaraf1230@gmail.com>
2017-11-07 07:35:22 +0100

    Added static Menubar , fixed bugs around it , removed Strut logo buttons , need to add their functionality , APP IS WORKING

--- app/bundles/app/strut.slide_editor/view/OperatingTable.js
+++ app/bundles/app/strut.slide_editor/view/OperatingTable.js
@@ -50,7 +50,7 @@ function(Backbone, empty, ComponentFactory, GlobalEvents, Component,
 			this._$slideContainer.css(config.slide.size);
 
 			DeckUtils.applyBackground(this._$slideContainer, this.model, this._deck, {transparentForSurface: true, surfaceForDefault: true, transparentForDeckSurface: true});
-			this._$markdownContent = $('<div class="markdownArea themedArea"></div>');
+			this._$markdownContent = $('<div id="slide_content" class="markdownArea themedArea"></div>');
 			this._$slideContainer.append(this._$markdownContent);
 
 			this._$slideContainer.selectable({
